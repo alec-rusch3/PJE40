97002a995789c6c3414789c0006d2dd69b3b8b9f
alec-rusch3 <46998794+alec-rusch3@users.noreply.github.com>
2019-05-10 13:52:14 +0100

    MPJ batch file
    Required to run MPJ files

---
+++ 825804/mpj.bat
@@ -0,0 +1,8 @@
+set MPJ_HOME=N:\mpj-v0_35P03
+set PATH=%MPJ_HOME%\bin;%PATH%
+set CLASSPATH=.;%MPJ_HOME%\lib\mpj.jar
+set PATH=C:\Program Files (x86)\Java\jre1.8.0_161\bin;%PATH%
+  
+cd /D E:\program\825804\build\classes
+
+cmd /K 
